28575c49edc429129d89152006dea9971b8d8944
Sergio Garcia <sergio.garcia@gtri.gatech.edu>
2018-02-21 18:59:30 -0500

    emacs | gscholar-bibtex command to obtain bibtex reference from different sources

---
+++ files_and_dirs/emacs.d/elpa/gscholar-bibtex-0.3.3/gscholar-bibtex-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gscholar-bibtex" "0.3.3" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :commit "ba4ce159e385d695d8560e8b06b3cbe48424861c" :keywords '("extensions"))
