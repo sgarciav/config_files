4661271df285f4dd3cf32460b28e8447828b0862
Sergio Garcia <sergio.garcia@gtri.gatech.edu>
2018-12-04 08:33:26 -0500

    emacs helm and cmake-mode updates

--- files_and_dirs/emacs.d/init.el
+++ files_and_dirs/emacs.d/init.el
@@ -5,6 +5,7 @@
 ;;; Code:
 ;; add settings directory to load path
 (add-to-list 'load-path "~/.emacs.d/settings")
+(require 'packages)
 
 ;; Helm Configuration for searching files (replaces ido-mode)
 (require 'helm)
@@ -21,7 +22,6 @@
 ;; setup extra config files (in settings directory)
 (require 'appearance)
 (require 'key-bindings)
-(require 'packages)
 (require 'bash-completion)
 (require 'latex-emacs-okular)
 
@@ -30,6 +30,11 @@
 ;; (setq ido-enable-flex-matching t)
 ;; (setq ido-everywhere t)
 ;; (ido-mode 1)
+
+;; cmake mode
+(require 'cmake-mode)
+(setq load-path (cons (expand-file-name "/dir/with/cmake-mode") load-path))
+;; (add-to-list 'auto-mode-alist '("\\CMakeLists\\.txt\\'" . cmake-mode))
 
 ;; Enable undo-tree-mode
 (require 'undo-tree)
@@ -158,3 +163,66 @@ If the new path's directories does not exist, create them."
 ;; end of file
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex -synctex=1")
+ '(TeX-command-list
+   (quote
+    (("XeLaTeX_SyncteX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil
+      (latex-mode doctex-mode)
+      :help "Run XeLaTeX")
+     ("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (plain-tex-mode texinfo-mode ams-tex-mode)
+      :help "Run plain TeX")
+     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
+      (latex-mode doctex-mode)
+      :help "Run LaTeX")
+     ("Makeinfo" "makeinfo %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with Info output")
+     ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with HTML output")
+     ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (ams-tex-mode)
+      :help "Run AMSTeX")
+     ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt once")
+     ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt until completion")
+     ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+     ("View" "%V" TeX-run-discard-or-function nil t :help "Run Viewer")
+     ("Print" "%p" TeX-run-command t t :help "Print the file")
+     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
+     ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
+     ("Check" "lacheck %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for correctness")
+     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+     ("Other" "" TeX-run-command t t :help "Run an arbitrary command")
+     ("Jump to PDF" "%V" TeX-run-discard-or-function nil t :help "Run Viewer"))))
+ '(TeX-modes
+   (quote
+    (tex-mode plain-tex-mode texinfo-mode latex-mode doctex-mode)))
+ '(column-number-mode t)
+ '(ido-enable-last-directory-history nil)
+ '(ido-max-work-directory-list 0)
+ '(ido-max-work-file-list 0)
+ '(ido-record-commands nil)
+ '(package-selected-packages
+   (quote
+    (cmake-mode helm magit yasnippet org-journal markdown-mode flymake-cppcheck flycheck cpputils-cmake company-c-headers cmake-project auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
