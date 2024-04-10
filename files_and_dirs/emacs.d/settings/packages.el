0a1dba81fd6f0dcb7b82ce794a1d401f03a89373
Sergio Garcia <sergiodotgarcia@gmail.com>
2024-04-10 13:21:41 -0400

    added elpa to the emacs config

--- files_and_dirs/emacs.d/settings/packages.el
+++ files_and_dirs/emacs.d/settings/packages.el
@@ -1,16 +1,16 @@
 ;;; package.el --- Contains all information about required packages.
-
 ;;; Commentary:
-(require 'package)
 
 ;;; Code:
 
+(require 'package)
 ;;(add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 ;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 ;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
 
 ;; list the packages you want
 (defvar package-list)
