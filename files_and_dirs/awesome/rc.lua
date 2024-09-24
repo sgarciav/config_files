b313275d221323cdf2c4e12c12d258d5389eac81
Sergio Garcia <sergiodotgarcia@gmail.com>
2024-09-24 16:16:25 -0400

    awesome: Allow for fair tile

--- files_and_dirs/awesome/rc.lua
+++ files_and_dirs/awesome/rc.lua
@@ -109,7 +109,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
+    awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
